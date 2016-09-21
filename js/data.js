eed68d048e7e37480a1d12df747b8602051b7bdc
Nils <nilsbultjer@gmail.com>
2016-09-21 10:27:11 +0200

    ...

--- js/data.js
+++ js/data.js
@@ -83,7 +83,7 @@ var haushaltsdaten;
 
 var xmlToJson = function(callback)
 {
-  d3.xml("data/Haushalt_2016.xml", function(error, xml) {
+  d3.xml("../data/Haushalt_2016.xml", function(error, xml) {
     json = xmlToJson2(xml);
     haushaltsdaten = json;
     if(callback)
