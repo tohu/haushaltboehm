15e391ddce275045865c86df1ed9a25df3903a71
Nils <nilsbultjer@gmail.com>
2016-09-24 13:42:36 +0200

    summen

--- app.js
+++ app.js
@@ -76,75 +76,130 @@ function recurse(node) {
 
 
 function parse(node) {
-  var tree = {text:"haushalt", children : []};
+  var ha = {text:"haushalt", children : [], wert:0};
   if(node.einzelplan) {
     node.einzelplan.forEach(function(plan){
-      var ei = {text : "EP: " + plan.text, children : []};
+      if(plan.kapitel.length < 1) {
+        console.log("Empty Einzelplan: " +  plan.text);
+      }
+
+      var ei = {text : "EP: " + plan.text, children : [], wert:0};
+      var eisum = 0;
+
       plan.kapitel.forEach(function(kapitel) {
-        // console.log("kapitel: " + kapitel.text)
-        var ka = {text : "KA: " + kapitel.text, children : []};
         if(!kapitel.ausgaben) {
           console.log("Keine Ausgaben in: " + kapitel.text);
           return;
         }
+        else if(kapitel.ausgaben.length < 1) {
+          console.log("Empty Kapitel: " +  kapitel.text);
+        }
+
+        var ka = {text : "KA: " + kapitel.text, children : [], wert:0};
+
         kapitel.ausgaben.forEach(function(ausgabe) {
+          //////////////////////////////////////////////////////////////////
+          function addToSum(obj, value) {
+            obj.wert += parseFloat(value);
+          }
+
           function processTitel(titel) {
             // console.log(titel.text);
             if(titel.soll.length > 1)
               console.log("LENGTH: " + titel.soll.length);
-            else if(!(titel.soll[0].wert)) {
-              // console.log("Kein Wert in: " + titel.text);
+            if(titel.soll[0].$.wert !== 0) {
+              if(!(titel.soll[0].$.wert)) {
+                console.log("Kein Wert in: " + titel.text);
+                console.log("Stattdessen: " +titel.soll[0].wert);
+              }
             }
-            var cur = {text : titel.text + ": " + titel.soll[0].$.wert};
+            var wert = titel.soll[0].$.wert;
+            addToSum(ka, wert);
+            addToSum(ei, wert);
+            addToSum(ha, wert);
+            var cur = {text : titel.text + ": " + wert, wert: wert};
             return cur;
           }
-          var au;
+          //////////////////////////////////////////////////////////////////
+          var entry_found;
+          // var easum = 0;
+          // var sonsum = 0;
+          // var tgsum = 0;
+
           if(ausgabe.einnahmen_ausgaben_art) {
+            entry_found = true;
             // console.log("ea art");
-            au = {text:"ea-art: ", children:[]};
+            var eas = {text:"ea-art: ", children:[], wert: 0};
+            var easum;
             ausgabe.einnahmen_ausgaben_art.forEach(function(ea_art) {
-              var aua = {text : ea_art.text, children : []};
+              easum = 0;
+              var ea = {text : ea_art.text, children : [], wert:0};
               ea_art.titel.forEach(function(titel) {
-                aua.children.push(processTitel(titel));
+                var t = processTitel(titel);
+                addToSum(ea, t.wert);
+                ea.children.push(t);
               });
-              au.children.push(aua);
+              addToSum(eas, ea.wert);
+              ea.text += " --- " + ea.wert;
+              eas.children.push(ea);
             });
+            eas.text += " --- " + eas.wert;
+            ka.children.push(eas);
           }
-          else if(ausgabe.titel) {
+          if(ausgabe.titel) {
+            entry_found = true;
             // console.log("titel");
-            au = {text:"sonstiges",children:[]};
+            var tit = {text:"sonstiges",children:[], wert: 0};
+            var sonsum = 0;
             ausgabe.titel.forEach(function(titel) {
-              au.children.push(processTitel(titel));
+              var t = processTitel(titel);
+              addToSum(tit, t.wert);
+              tit.children.push(t);
             });
+            tit.text += " --- " + tit.wert;
+            ka.children.push(tit);
           }
-          else if(ausgabe.titelgruppe) {
-            au = {text: "titelgruppen", children:[]};
+          if(ausgabe.titelgruppe) {
+            entry_found = true;
+            var tgs = {text: "titelgruppen", children:[], wert: 0};
             if(ausgabe.titelgruppe.text) {
               console.log(ei.text + ".." + ka.text + ".." + tg.text);
             }
+
+
             ausgabe.titelgruppe.forEach(function(titelgruppe) {
-              var tg = {text : titelgruppe.text, children : []};
+              var tg = {text : titelgruppe.text, children : [], wert:0};
               titelgruppe.titel.forEach(function(titel) {
-                tg.children.push(processTitel(titel));
+                var t = processTitel(titel);
+                addToSum(tg, t.wert);
+                tg.children.push(t);
               });
-              au.children.push(tg);
+              addToSum(tgs, tg.wert);
+              tg.text += " --- " + tg.wert;
+              tgs.children.push(tg);
             });
+            tgs.text += " --- " + tgs.wert;
+            ka.children.push(tgs);
           }
-          else {
-            console.log("Unexpected in  kapitel.ausgaben: " + JSON.stringify(ausgabe, null, 2));
+          if (!entry_found) {
+            console.log("No entry in  plan:" + plan.text +" --- kapitel: " + ka.text);
+            console.log(ausgabe);
             return;
           }
-          ka.children.push(au);
+          // ka.children.push(au);
         });
+        ka.text += " --- " + ka.wert;
         ei.children.push(ka);
       });
-      tree.children.push(ei);
+      ei.text += " --- " + ei.wert;
+      ha.children.push(ei);
     });
   }
   else {
     console.log("Unexpected root: " + node);
   }
-  return tree;
+  ha.text += " --- " + ha.wert;
+  return ha;
 }
 tree = parse(haushaltobj.haushalt);
 var haushaltjsonpathtree = 'public/data/haushalt_2016tree.json';
